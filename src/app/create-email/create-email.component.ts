5fedb4bbe1d68a97dfb866567a66b241ee606777
Trey Crossley <treycrossley@gmail.com>
2024-01-20 04:34:59 -0800

    initial UI for email sending"

--- src/app/create-email/create-email.component.ts
+++ src/app/create-email/create-email.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core'
+import { FormControl, ReactiveFormsModule, FormGroup } from '@angular/forms'
+import { Apollo, gql } from 'apollo-angular'
 
 @Component({
   selector: 'app-create-email',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-email.component.html',
   styleUrl: './create-email.component.scss',
 })
-export class CreateEmailComponent {}
+export class CreateEmailComponent {
+  constructor(private apollo: Apollo) {}
+
+  SEND_EMAILS = gql`
+    mutation sendEmail($recipient: String, $subject: String, $text: String) {
+      sendEmail(recipient: $recipient, subject: $subject, text: $text) {
+        code
+        message
+        success
+      }
+    }
+  `
+
+  emailConstructor = new FormGroup({
+    recipient: new FormControl(''),
+    subject: new FormControl(''),
+    body: new FormControl(''),
+  })
+
+  onSubmit() {
+    //TODO
+    console.log(this.emailConstructor.value)
+    this.apollo
+      .mutate({
+        mutation: this.SEND_EMAILS,
+        variables: {
+          recipient: this.emailConstructor.value.recipient,
+          subject: this.emailConstructor.value.subject,
+          body: this.emailConstructor.value.body,
+        },
+      })
+      .subscribe(
+        ({ data }: any) => {
+          console.log(data)
+        },
+        err => {
+          console.error(err)
+        }
+      )
+  }
+}
