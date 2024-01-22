10919c37ce858500ff6acc982a86e027d59b3552
Trey Crossley <treycrossley@gmail.com>
2024-01-22 03:31:13 -0800

    validating that a valid email will be sent

--- src/app/create-email/create-email.component.ts
+++ src/app/create-email/create-email.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import {
   FormControl,
   ReactiveFormsModule,
   FormGroup,
   Validators,
+  AbstractControl,
 } from '@angular/forms'
 import { Apollo, gql } from 'apollo-angular'
 import { MatInputModule } from '@angular/material/input'
 import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatSelectModule } from '@angular/material/select'
 
 import { MatButtonModule } from '@angular/material/button'
 import { MatToolbarModule } from '@angular/material/toolbar'
@@ -15,8 +17,15 @@ import { CommonModule } from '@angular/common'
 import { SingleFileUploadComponent } from '../single-file-upload/single-file-upload.component'
 
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { Subscription } from 'rxjs'
 
-import { ValidateEmailList } from '../services/email.validator'
+import { ValidateEmailString } from '../services/email.validator'
+
+interface group {
+  id: string
+  recipients: string
+  name: string
+}
 
 @Component({
   selector: 'app-create-email',
@@ -30,18 +39,46 @@ import { ValidateEmailList } from '../services/email.validator'
     CommonModule,
     MatProgressSpinnerModule,
     SingleFileUploadComponent,
+    MatSelectModule,
   ],
   templateUrl: './create-email.component.html',
   styleUrls: ['../app.component.scss', './create-email.component.scss'],
 })
-export class CreateEmailComponent {
+export class CreateEmailComponent implements OnInit, OnDestroy {
   emailSent: boolean = false
   isErr: boolean = false
   showEmailForm: boolean = true
   loading: boolean = false
   file: File | undefined = undefined // Variable to store file
+  groupList: group[] = []
+  private querySubscription: Subscription
 
   constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.querySubscription = this.apollo
+      .watchQuery<any>({
+        query: this.GET_EMAIL_GROUPS,
+      })
+      .valueChanges.subscribe(({ data, loading }) => {
+        this.loading = loading
+        this.groupList = data.getGroups
+      })
+  }
+
+  ngOnDestroy() {
+    this.querySubscription.unsubscribe()
+  }
+
+  GET_EMAIL_GROUPS = gql`
+    query Query {
+      getGroups {
+        recipients
+        name
+        id
+      }
+    }
+  `
 
   SEND_EMAILS = gql`
     mutation sendEmail(
@@ -63,11 +100,28 @@ export class CreateEmailComponent {
     }
   `
 
-  emailConstructor = new FormGroup({
-    recipient: new FormControl('', [Validators.required, ValidateEmailList]),
-    subject: new FormControl('', [Validators.required]),
-    body: new FormControl('', [Validators.required]),
-  })
+  emailConstructor = new FormGroup(
+    {
+      email_groups: new FormControl(''),
+      recipient: new FormControl(''),
+      subject: new FormControl('', [Validators.required]),
+      body: new FormControl('', [Validators.required]),
+    },
+    this.emailExistsValidator
+  )
+
+  emailExistsValidator(control: AbstractControl) {
+    const validator = { invalidInput: true }
+    const { recipient, email_groups } = control.value
+    const recipientsExist: boolean = recipient ? true : false
+    const groupsExist =
+      email_groups === undefined || email_groups.length == 0 ? false : true
+
+    if (!recipientsExist && !groupsExist) return validator
+    if (ValidateEmailString(recipient) != null && !groupsExist) return validator
+
+    return null
+  }
 
   getFile(file: File): void {
     this.file = file
@@ -82,21 +136,38 @@ export class CreateEmailComponent {
     })
   }
 
-  async onSubmit() {
-    this.loading = true
+  getTotalRecipients() {
+    const recipients = this.emailConstructor.value.recipient?.trim().split(',')
+    const emailGroups = this.emailConstructor.value.email_groups
+    if (emailGroups?.length !== undefined) {
+      for (let i = 0; i < emailGroups?.length; i++) {
+        recipients?.push(emailGroups[i])
+      }
+    }
+    const uniqueList = [...new Set(recipients)]
+    return uniqueList.toString()
+  }
 
+  async getFiles() {
     const files = []
     let path = null
     if (this.file) {
       path = await this.file2Base64(this.file)
       files.push({ filename: this.file?.name, path: path, encoding: 'base64' })
     }
+    return files
+  }
+
+  async onSubmit() {
+    this.loading = true
+    const recipients = this.getTotalRecipients()
+    const files = await this.getFiles()
 
     this.apollo
       .mutate({
         mutation: this.SEND_EMAILS,
         variables: {
-          recipient: this.emailConstructor.value.recipient,
+          recipient: recipients,
           subject: this.emailConstructor.value.subject,
           text: this.emailConstructor.value.body,
           attachments: files,
--- src/app/services/email.validator.ts
+++ src/app/services/email.validator.ts
@@ -1,8 +1,25 @@
 import { AbstractControl } from '@angular/forms'
 
+const emailRegex = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'
+
 export function ValidateEmailList(control: AbstractControl) {
-  const emailRegex = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'
   const emailList: Array<string> = control.value.split(',')
+  const regex = new RegExp(emailRegex)
+
+  let validator = null
+  for (const email of emailList) {
+    const mail = email.trim()
+    if (!regex.test(mail)) {
+      validator = { invalidUrl: true }
+      break
+    }
+  }
+
+  return validator
+}
+
+export function ValidateEmailString(emailString: string) {
+  const emailList: Array<string> = emailString.split(',')
   const regex = new RegExp(emailRegex)
 
   let validator = null
